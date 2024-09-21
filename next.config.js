dd46bc57503622dcd7f8d5a91e185e9e9acf69f7
casefox-dev <nandeesh@casefox.in>
2024-09-22 03:37:04 +0530

    Fix router

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: '/casefox.in/',
-  basePath: '/casefox.in',
+  // Comment out or remove the following lines for development
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
+  // assetPrefix: '/casefox.in/',
+  // basePath: '/casefox.in',
 }
 
 module.exports = nextConfig
